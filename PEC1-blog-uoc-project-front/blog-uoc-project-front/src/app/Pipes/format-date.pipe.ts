3a177773246b005395767b1ec6c33f59ad3939ec
Martro96 <martarodrigo.55@gmail.com>
2025-03-05 20:01:25 +0100

    Añado pipe ejercicio 8

--- PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Pipes/format-date.pipe.ts
+++ PEC1-blog-uoc-project-front/blog-uoc-project-front/src/app/Pipes/format-date.pipe.ts
@@ -4,8 +4,31 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatDate',
 })
 export class FormatDatePipe implements PipeTransform {
-  transform(value: Date, ...args: number[]): unknown {
+  transform(value: Date, formatDate: number): any {
     // TODO 1
-    return null;
+    if (!value) return '';
+
+    const date = new Date(value);
+    const day = date.getDate().toString().padStart(2,'0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    switch (formatDate) {
+      case 1: 
+      return `${day}${month}${year}`;
+
+      case 2: 
+      return `${day} / ${month} / ${year}`;
+    
+      case 3:
+      return `${day}/${month}/${year}`;
+
+      case 4: 
+      return `${year}-${month}-${day}`;
+
+      default: 
+      return `${day}/${month}/${year}`;
+
+    }
   }
 }
